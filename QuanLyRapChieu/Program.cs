baecd14e6b2295f1d6ea155fe1cde9343a729d0d
bichthuy117 <bichthuy144com@gmail.com>
2025-04-03 18:11:37 +0700

    Create frmLogin, frmTheater

--- QuanLyRapChieu/Program.cs
+++ QuanLyRapChieu/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyRapChieu
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmCustomer());
+            Application.Run(new frmTheatre());
         }
     }
 }
