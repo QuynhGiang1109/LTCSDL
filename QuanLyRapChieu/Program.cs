2cfd2a47b8b85f78e1aa13cfe65847b9e2637259
bichthuy117 <bichthuy144com@gmail.com>
2025-04-16 21:04:19 +0700

    Thuy push code 3 lop

--- QuanLyRapChieu/Program.cs
+++ QuanLyRapChieu/Program.cs
@@ -17,7 +17,7 @@ namespace QuanLyRapChieu
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new frmAdmin());
+            Application.Run(new frmLogin());
          
         }
     }
--- QuanLyRapChieu/frmLogin.cs
+++ QuanLyRapChieu/frmLogin.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,10 +17,15 @@ namespace QuanLyRapChieu
 {
     public partial class frmLogin : Form
     {
+        // Biến để lưu trạng thái ẩn/hiện mật khẩu
+        private bool isPasswordVisible = false;
+
         public frmLogin()
         {
             InitializeComponent();
+            this.Load += frmLogin_Load;
         }
+        
         private void btnLogin_Click(object sender, EventArgs e)
         {
             string userName = txtUsername.Text; //user qly: admin, nvien: NV02
@@ -30,10 +36,10 @@ namespace QuanLyRapChieu
             if (taiKhoan != null)
             {
                 // Đăng nhập thành công
-                //frmDashboard dashboard = new frmDashboard(taiKhoan);
-                //this.Hide();
-                //dashboard.ShowDialog();
-                //this.Show();
+                frmDashboard dashboard = new frmDashboard(taiKhoan);
+                this.Hide();
+                dashboard.ShowDialog();
+                this.Show();
 
                 // Xóa thông tin đăng nhập sau khi đóng Dashboard
                 txtUsername.Clear();
@@ -57,6 +63,49 @@ namespace QuanLyRapChieu
             }
         }
 
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            // Đường dẫn đầy đủ từ thư mục chạy chương trình
+            string eyeClosedPath = Path.Combine(Application.StartupPath, "image", "close_eye.png");
+            string eyeOpenPath = Path.Combine(Application.StartupPath, "image", "open_eye.png");
+
+            // Kiểm tra xem ảnh có tồn tại không trước khi load
+            if (File.Exists(eyeClosedPath))
+            {
+                this.pnEye.BackgroundImage = Image.FromFile(eyeClosedPath);
+                this.pnEye.BackgroundImageLayout = ImageLayout.Zoom;
+            }
+            else
+            {
+                MessageBox.Show("Không tìm thấy ảnh eye_closed.png");
+            }
+
+        }
+        private void pnEye_Click(object sender, EventArgs e)
+        {
+            string eyeClosedPath = Path.Combine(Application.StartupPath, "image", "close_eye.png");
+            string eyeOpenPath = Path.Combine(Application.StartupPath, "image", "open_eye.png");
+
+            if (isPasswordVisible) //show password
+            {
+                txtPassword.UseSystemPasswordChar = true; //neu dang hien pw
+
+                if (File.Exists(eyeClosedPath))
+                {
+                    pnEye.BackgroundImage = Image.FromFile(eyeClosedPath); //doi img sang hide pw
+                }
+                isPasswordVisible = false; //che do an mkhau
+            }
+            else
+            {
+                txtPassword.UseSystemPasswordChar = false;
+                if (File.Exists(eyeOpenPath))
+                {
+                    pnEye.BackgroundImage = Image.FromFile(eyeOpenPath);
+                }
+                isPasswordVisible = true;
+            }
+        }
     }
 }
 
